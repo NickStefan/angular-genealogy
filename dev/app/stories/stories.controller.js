5c56ca59dca75887a5bd21ed54276a6b80dfeb44
Nick Stefan <NickStefan12@gmail.com>
2014-12-06 11:13:19 -0800

    lorem ipsum refactored

--- dev/app/stories/stories.controller.js
+++ dev/app/stories/stories.controller.js
@@ -5,14 +5,44 @@ angular
 storiesController.$inject = ['$scope'];
 
 function storiesController($scope){
-  $scope.stories = [
-    [{'text':'link1'},{'text':'link2'}],
-    [{'text':'link3'},{'text':'link4'}],
-  ]
+  $scope.stories = _.map(new Array(5),function(){
+    return {
+      viewing: false,
+      links: _.map(new Array(10),function(v,k,c){
+        return {
+          viewing: false,
+          text:_.map(new Array(10),function(){ 
+            return Math.random().toString(36).slice(2);
+          }).join(" ")
+        };
+      })
+    };
+  });
+
+  // actual future data structure:
+  // [
+  //   {
+    //  viewing: false,
+    //  links: [
+    //     {'text':'link1','viewing':false},
+    //     {'text':'link2','viewing':false},
+    //   ]
+    //  },
+    // {
+    //  viewing: false,
+    //  links:
+    //   [
+    //     {'text':'link1','viewing':false},
+    //     {'text':'link2','viewing':false},
+    //   ]
+    //  }
+  // ]
+
   $scope.linkedDoc = linkedDoc;
-};
+}
 
 function linkedDoc(storyIndex,linkIndex){
-  console.log(storyIndex,"storyIndex")
-  console.log(linkIndex,"linkIndex")
-};
+  console.log(storyIndex,"storyIndex");
+  console.log(linkIndex,"linkIndex");
+  $scope.stories[storyIndex][linkIndex].viewing = false;
+}
